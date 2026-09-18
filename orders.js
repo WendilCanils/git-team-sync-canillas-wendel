dc5e3fd46d91e4401fa76618eb12c42a55a2c3bf
LAB11_USER ON WS11 <L11X09W11@LabHCI.net>
2026-09-18 09:28:55 +0800

    Round base loyalty points before VIP bonus - canillas.wendel

--- orders.js
+++ orders.js
@@ -16,7 +16,7 @@ function cancelOrder(order) {
 }
 
 function calculateLoyaltyPoints(order) {
-  let points = order.total / 10;
+  let points = Math.round(order.total / 10);
 
   if (order.total > 100) {
     points = points * 1.5;
@@ -24,5 +24,4 @@ function calculateLoyaltyPoints(order) {
 
   return Math.round(points);
 }
-
 module.exports = { createOrder, applyDiscount, cancelOrder, calculateLoyaltyPoints };
